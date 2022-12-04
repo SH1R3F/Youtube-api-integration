3785f1b2d428418aa2acb32a7983b539ce331e8a
Mahmoud Shiref <sh1r3f@gmail.com>
2022-12-04 15:02:17 +0200

    Modify general layout of dashboard
    - Youtube section is created
    - Dashboard layout is updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,14 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+        <div class="container">
+            <div class="grid grid-cols-4">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg py-3 px-4">
+                    <a href="/youtube">
+                        <h1 class="text-3xl text-gray-700 mb-3">Youtube</h1>
+                        <p class="text-gray-700 mb-3">Here you can find youtube scripts</p>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::view('/youtube', 'youtube')->name('youtube');
 });
 
 
